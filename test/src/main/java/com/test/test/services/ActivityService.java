df4b60882058da117eae1538b0b6e5a8d4f36e13
Charlie <111837196+charlie-zhongmin@users.noreply.github.com>
2022-10-04 18:43:12 +1000

    Add files via upload

---
+++ test/src/main/java/com/test/test/services/ActivityService.java
@@ -0,0 +1,12 @@
+package com.test.test.services;
+
+import com.test.test.entities.Activity;
+
+public interface ActivityService {
+
+    void saveActivity(Activity activity);
+
+    Activity getActivityByDate(String date);
+
+    String getLocalActivity(String Date);
+}
---
+++ test/src/main/java/com/test/test/services/ActivityServiceImpl.java
@@ -0,0 +1,36 @@
+package com.test.test.services;
+
+import com.test.test.entities.Activity;
+import com.test.test.repositories.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.security.InvalidParameterException;
+import java.util.NoSuchElementException;
+
+@Service
+public class ActivityServiceImpl implements ActivityService {
+
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    @Override
+    public void saveActivity(Activity activity) {
+        activityRepository.save(activity);
+    }
+
+    @Override
+    public Activity getActivityByDate(String date) throws NoSuchElementException {
+        return activityRepository.findByDate(date)
+                .orElseThrow(() -> new NoSuchElementException("Date "+ date + " not found"));
+    }
+
+    //Factory of activity getter
+    @Override
+    public String getLocalActivity(String date) {
+        Factory factory = new Factory();
+        return factory.getActivity(date).getActivity();
+    }
+
+}
+
